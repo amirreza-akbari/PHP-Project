0138436db3533578f4b798164af03cbd879ccdce
amirrezaakbari <123993165+amirreza-akbari@users.noreply.github.com>
2023-12-29 12:56:38 -0800

    Add files via upload

---
+++ js/captcha.JS
@@ -0,0 +1,23 @@
+function generateCaptcha() {
+  var num1 = Math.floor(Math.random() * 10) + 1;
+  var num2 = Math.floor(Math.random() * 10) + 1;
+  var captchaText = num1 + " + " + num2 + " = ?";
+  document.getElementById("captchaImage").innerHTML = captchaText;
+  window.captchaResult = num1 + num2;
+}
+
+function checkCaptcha() {
+  var userInput = document.getElementById("userInput").value;
+  var isNumeric = /^\d+$/;
+
+  if (isNaN(userInput.trim()) || parseInt(userInput) !== window.captchaResult) {
+
+    alert('کد امنیتی اشتباه است لطفا حاصل عبارت را صحیح وارد کنید');
+    generateCaptcha();
+    return false; // Prevent the form from submitting
+  }
+
+ }
+
+
+generateCaptcha();
---
+++ php/show.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+
+
+if (isset($_SESSION['user_type']) && isset($_SESSION['username']) && isset($_SESSION['password']) &&
+	!empty($_SESSION['user_type']) && !empty($_SESSION['username']) && !empty($_SESSION['password'])) {
+
+	$user_type = $_SESSION['user_type'];
+    $username = $_SESSION['username'];
+	$password = $_SESSION['password'];
+
+    $con = mysqli_connect("localhost", "root", "", "robot_live");
+    $query = "SELECT filename FROM users WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($con, $query);
+
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $filename = $row['filename'];
+
+       
+        $_SESSION['username'] = $username;
+        $_SESSION['filename'] = $filename;
+    } else {
+		exit("<h1 style=color:red;text-align:center;>نام کاربری یا رمز عبور نامعتبر است<br /><span>لطفا نام کاربری و رمز عبورا به درستی وارد کنید</span><br /><br /> <a href='../index.html'>بازگشت به صفحه‌ی اصلی</a></h1>");
+    }
+
+    mysqli_close($con);
+} else {
+	exit("<h1 style=color:red;text-align:center;>نام کاربری یا رمز عبور نامعتبر است<br /><span>لطفا نام کاربری و رمز عبورا به درستی وارد کنید</span><br /><br /> <a href='../index.html'>بازگشت به صفحه‌ی اصلی</a></h1>");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>دریافت کارنامه -هنرستان راهبرد</title>
+	<link type="text/css" href="../css/bootstrap.min.css" rel="stylesheet" />
+	<link type="text/css" href="../css/font-awesome.min.css" rel="stylesheet" />
+	<link type="text/css" href="../css/util.css" rel="stylesheet" />
+	<link type="text/css" href="../css/style.css" rel="stylesheet" />
+</head>
+<body class="rtl">
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-form-title" style="background-image: url(../pics/02.jpeg);">
+					<span class="login100-form-title-1">جهت دریافت کارنامه بر روی لینک دانلود کلیک کنید</span>
+				</div>
+				<br />
+					<a href="../pdf/<?php echo $filename; ?>" download="<?php echo $filename; ?>" class="login100-form-btn">دانلود</a><br />
+					<a href="../php/logout.php" class="login100-form-btn">خروج</a>
+				<br />
+			</div>
+		</div>
+	</div>
+
+	<script src="../js/jquery-3.1.1.min.js"></script>
+	<script  src="../js/scripts.js"></script>
+</body>
+</html>
